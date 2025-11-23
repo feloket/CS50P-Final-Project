459e7729ebbb57b807b35f73a70fdc8272e65fb3
Daniel Garcia <danielggarcia7@gmail.com>
2025-11-23 14:57:25 -0300

    Example structure placed

---
+++ project.py
@@ -0,0 +1,19 @@
+# still need to think about a prject
+def main():
+    ...
+
+
+def function_1():
+    ...
+
+
+def function_2():
+    ...
+
+
+def function_n():
+    ...
+
+
+if __name__ == "__main__":
+    main()
